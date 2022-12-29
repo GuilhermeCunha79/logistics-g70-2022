edd800724a0cd3b90af02c83c2d39cd902d90f96
Hugo Carvalho <1210813@isep.ipp.pt>
2022-12-29 03:23:26 +0000

    LP-83 (Implementation) Created User repo

---
+++ src/repos/userRepo.ts
@@ -0,0 +1,53 @@
+import {Inject, Service} from 'typedi';
+import {Document, Model} from 'mongoose';
+import {User} from "../domain/User/user";
+import IUserRepo from "./IRepos/IUserRepo";
+import {IUserPersistence} from "../dataschema/IUserPersistence";
+import { UserMap } from "../mappers/userMap";
+
+@Service()
+export default class UserRepo implements IUserRepo {
+
+	constructor(@Inject('userSchema') private userSchema: Model<IUserPersistence & Document>) {
+	}
+
+	exists(t: User): Promise<boolean> {
+		throw new Error('Method not implemented.');
+	}
+
+	public async save(user: User): Promise<User>{
+		const query = {email: user.email.value};
+		const userDocument = await this.userSchema.findOne(query);
+
+		try {
+			if (userDocument === null) {
+				const rawUser: any = UserMap.toPersistence(user);
+				const userCreated = await this.userSchema.create(rawUser);
+				return UserMap.toDomain(userCreated);
+			} else {
+				userDocument.email = user.email.value;
+				userDocument.password = user.password.value;
+				userDocument.phoneNumber = user.phoneNumber.value;
+				userDocument.role = user.role.value;
+
+				await userDocument.save();
+				return user;
+			}
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	public async find(query?: any): Promise<User[]>{
+		const userRecord = await this.userSchema.find(query);
+
+		if (userRecord != null) {
+			return (userRecord.map((postRecord) => UserMap.toDomain(postRecord)));
+		}
+		return null;
+	}
+
+	public async delete(userId: string): Promise<User>{
+		throw new Error('Method not implemented.');
+	}
+}
