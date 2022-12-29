0939297bb994fb931400363f52a5112c02f33702
Hugo Carvalho <1210813@isep.ipp.pt>
2022-12-29 03:23:39 +0000

    LP-83 (Implementation) Created User mapper

---
+++ src/mappers/userMap.ts
@@ -0,0 +1,46 @@
+import { Mapper } from "../core/infra/Mapper";
+import { Document, Model } from "mongoose";
+import { UniqueEntityID } from "../core/domain/UniqueEntityID";
+
+import { User } from "../domain/User/user";
+import IUserDTO from "../dto/IUserDTO";
+import { IUserPersistence } from "../dataschema/IUserPersistence";
+import { UserEmail } from "../domain/User/userEmail";
+import { UserPassword } from "../domain/User/userPassword";
+import { UserPhoneNumber } from "../domain/User/userPhoneNumber";
+import { UserRole } from "../domain/User/userRole";
+
+export class UserMap extends Mapper<User> {
+
+	public static toDTO(user: User): IUserDTO {
+		return {
+			email: user.email.value,
+			password: user.password.value,
+			phoneNumber: user.phoneNumber.value,
+			role: user.role.value
+		} as IUserDTO;
+	}
+
+	public static toDomain(raw: any | Model<IUserPersistence & Document>): User {
+		const userOrError = User.create({
+			email: UserEmail.create(raw.email).getValue(),
+			password: UserPassword.create(raw.password).getValue(),
+			phoneNumber: UserPhoneNumber.create(raw.phoneNumber).getValue(),
+			role: UserRole.create(raw.role).getValue()
+			}, new UniqueEntityID(raw.domainId)
+		);
+
+		userOrError.isFailure ? console.log(userOrError.error) : "";
+		return userOrError.isSuccess ? userOrError.getValue() : null;
+	}
+
+	public static toPersistence(user: User): any {
+		return {
+			domainId: user.id.toString(),
+			email: user.email.value,
+			password: user.password.value,
+			phoneNumber: user.phoneNumber.value,
+			role: user.role.value
+		};
+	}
+}
